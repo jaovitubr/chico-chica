75c5f0c7f1abc263bd73c9283fbd0b59e0538161
jaovitubr <joaovmachado10@gmail.com>
2025-11-15 18:46:12 -0300

    adicionado outros produtos

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -25,7 +25,10 @@ const products = [
   { id: 24, name: "Travessas", image: "/images/products/travessas.webp" },
   { id: 25, name: "Colher (conchas)", image: "/images/products/conchas.webp" },
   { id: 26, name: "Escorredor de talher e louças", image: "/images/products/escorredor-talheres.webp" },
-  { id: 27, name: "Panela pipoqueira", image: "/images/products/panela-pipoqueira.webp" }
+  { id: 27, name: "Panela pipoqueira", image: "/images/products/panela-pipoqueira.webp" },
+  { id: 28, name: "Jogo de talheres", image: "/images/products/jogo-talheres.webp" },
+  { id: 29, name: "Sanduicheira", image: "/images/products/sanduicheira.webp" },
+  { id: 20, name: "Processador de alimentos", image: "/images/products/processador-alimentos.webp" },
 ];
 
 class ProductInjector {
